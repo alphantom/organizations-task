630093b629110e14eb55008ceb897ac4028cec3c
Albina <al.phant@yandex.ru>
2019-09-11 18:12:43 +0400

    Created entities

---
+++ src/main/java/com/albina/springproject/models/Organization.java
@@ -0,0 +1,99 @@
+package com.albina.springproject.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "organization")
+public class Organization {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column(length = 50)
+    private String name;
+
+    @Column(name = "fullname")
+    private String fullName;
+
+    @Column(length = 12, nullable = false, unique = true)
+    private String inn;
+
+    @Column(length = 9, nullable = false)
+    private String kpp;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(length = 15)
+    private String phone;
+
+    @Column(name = "active", nullable = false)
+    private boolean isActive;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getInn() {
+        return inn;
+    }
+
+    public void setInn(String inn) {
+        this.inn = inn;
+    }
+
+    public String getKpp() {
+        return kpp;
+    }
+
+    public void setKpp(String kpp) {
+        this.kpp = kpp;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+}
