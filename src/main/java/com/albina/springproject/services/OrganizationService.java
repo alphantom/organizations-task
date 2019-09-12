d57b8c525cfeaad01e78ec6a1d5cc2a8a63ac639
Albina <al.phant@yandex.ru>
2019-09-12 17:09:47 +0400

    Cleared import statements

--- src/main/java/com/albina/springproject/services/OrganizationService.java
+++ src/main/java/com/albina/springproject/services/OrganizationService.java
@@ -3,10 +3,6 @@ package com.albina.springproject.services;
 import com.albina.springproject.view.OrganizationView;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.Valid;
-import java.util.List;
-import java.util.Map;
-
 @Validated
 public interface OrganizationService extends CrudService<OrganizationView>{
 
