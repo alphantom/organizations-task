07ac01b9e879613f3d46716b67da9f8e193e1e62
Albina <al.phant@yandex.ru>
2019-09-12 17:14:40 +0400

    Added Office api

---
+++ src/main/java/com/albina/springproject/controllers/OfficeController.java
@@ -0,0 +1,51 @@
+package com.albina.springproject.controllers;
+
+import com.albina.springproject.common.DataResponse;
+import com.albina.springproject.common.ResultResponse;
+import com.albina.springproject.services.OfficeService;
+import com.albina.springproject.view.OfficeView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping(value = "/api/office", produces = APPLICATION_JSON_VALUE)
+public class OfficeController {
+
+    private final OfficeService officeService;
+
+    @Autowired
+    public OfficeController(OfficeService officeService) {
+        this.officeService = officeService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getById(@PathVariable Long id) {
+        return new ResponseEntity<>(new DataResponse<>(officeService.get(id)), HttpStatus.OK);
+    }
+
+    @PostMapping("/list")
+    public ResponseEntity list(@RequestBody Map<String, Object> requestBody) {
+        return new ResponseEntity<>(new DataResponse<>(officeService.getFilteredList(requestBody)), HttpStatus.OK);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity save(@RequestBody OfficeView office) {
+
+        officeService.add(office);
+        return new ResponseEntity<>(new DataResponse<>(new ResultResponse("success")), HttpStatus.OK);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity update(@RequestBody OfficeView office) {
+
+        officeService.add(office);
+        return new ResponseEntity<>(new DataResponse<>(new ResultResponse("success")), HttpStatus.OK);
+    }
+
+}
--- src/main/java/com/albina/springproject/repositories/OfficeRepository.java
+++ src/main/java/com/albina/springproject/repositories/OfficeRepository.java
@@ -1,7 +1,9 @@
 package com.albina.springproject.repositories;
 
 import com.albina.springproject.models.Office;
+import com.albina.springproject.models.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface OfficeRepository extends JpaRepository<Office, Long> {
+public interface OfficeRepository extends JpaRepository<Office, Long>, JpaSpecificationExecutor<Office> {
 }
