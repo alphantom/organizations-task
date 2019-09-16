e89fa9858e4dcb8831defd442804dbfd7fecb3f4
Albina <al.phant@yandex.ru>
2019-09-16 11:58:48 +0400

    Added catalog's repositories

--- src/main/java/com/albina/springproject/repositories/catalog/CountryRepository.java
+++ src/main/java/com/albina/springproject/repositories/catalog/CountryRepository.java
@@ -2,6 +2,7 @@ package com.albina.springproject.repositories.catalog;
 
 import com.albina.springproject.models.catalog.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface CountryRepository extends JpaRepository<Country, Long> {
+public interface CountryRepository extends JpaRepository<Country, Short>, JpaSpecificationExecutor<Country> {
 }
--- src/main/java/com/albina/springproject/repositories/catalog/DocumentRepository.java
+++ src/main/java/com/albina/springproject/repositories/catalog/DocumentRepository.java
@@ -2,6 +2,7 @@ package com.albina.springproject.repositories.catalog;
 
 import com.albina.springproject.models.catalog.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface DocumentRepository extends JpaRepository<Document, Long> {
+public interface DocumentRepository extends JpaRepository<Document, Byte>, JpaSpecificationExecutor<Document> {
 }
