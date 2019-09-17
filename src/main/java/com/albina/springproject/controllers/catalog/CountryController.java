6e9c77945af363a225d7bbf87ff211ca97489e1b
Albina <al.phant@yandex.ru>
2019-09-17 11:33:25 +0400

    Added RequestMapping to catalogue's controllers

--- src/main/java/com/albina/springproject/controllers/catalog/CountryController.java
+++ src/main/java/com/albina/springproject/controllers/catalog/CountryController.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @RestController
+@RequestMapping(value = "/api", produces = APPLICATION_JSON_VALUE)
 public class CountryController {
 
     @Autowired
--- src/main/java/com/albina/springproject/controllers/catalog/DocumentController.java
+++ src/main/java/com/albina/springproject/controllers/catalog/DocumentController.java
@@ -6,10 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 
 @RestController
+@RequestMapping(value = "/api", produces = APPLICATION_JSON_VALUE)
 public class DocumentController {
 
     @Autowired
